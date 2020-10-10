3f51101145c290698cdc5fd9656155c932388173
kelvin-daniel <kaymutor@gmail.com>
2020-10-10 16:17:46 +0300

    add index view

--- my_gram/tests.py
+++ my_gram/tests.py
@@ -1,32 +1,63 @@
 from django.test import TestCase
-from .models import Image,Category,Location
+from models import Image,Category,Location
 
 # tests
 class TestImage(TestCase):
     def setUp(self):
         #location test
-        self.location=Location(name='Mali')
-        self.location.save_location()
+        self.locashon=Location(name='Mali')
+        self.locashon.save_location()
         #category test
-        self.category= Category(category='art')
-        self.category.save_category()
+        self.categry= Category(category='art')
+        self.categry.save_category()
         #image test
-        self.image = Image(name='creekidy', description='creekidy rock on creek road', category=self.category, location=self.location)
+        self.image = Image(name='creekidy', description='creekidy rock on creek road', category=self.categry, location=self.locashon)
         self.image.save_image()
 
     def test_save_image(self):
         self.image.save()
         images = Image.objects.all()
         self.assertTrue(len(images)>0)
-    
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+
     def test_delete_image(self):
         self.image.delete()
         images = Image.objects.all()
         self.assertTrue(len(images)==0)
-    def test_update_image(self):
-        pass
-    def test_get_image_by_id(self)
+
+    def tearDown(self):
+        self.image.delete_image()
+        self.categry.delete_category()
+        self.locashon.delete_location()
+
+    def test_get_all_images(self):
+        images = Image.get_all_images()
+        self.assertTrue(len(images)>0)
     
+    def test_get_image_by_id(self):
+        images= Image.get_image_by_id(self.image.id)
+        self.assertTrue(len(images) == 1)
+
+    def test_search_by_category(self):
+        images = Image.search_by_category('art')
+        self.assertTrue(len(images)>0)
+
+    def test_filter_by_location(self):
+        images = Image.fil0ter_by_location('1')
+        print(images)
+        self.assertTrue(len(images)>0)
+    
+    # def test_update_image(self):
+    #     self.image.save_image()
+    #     image = Image.update_image( self.image.id, 'test update', 'my test',self.loc, self.cat)
+    #     upimage = Image.objects.filter(id = self.image.id)
+    #     print(upimage)
+    #     self.assertTrue(Image.name == 'test update')
+
+
+
 class CategoryTestClass(TestCase):
     def setUp(self):
         self.categry = Category(name="art")
--- my_gram/views.py
+++ my_gram/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
-from models import Image
+from django.shortcuts import render,redirect
+from models import Image,Category,Location
+from django.http import HttpResponse,Http404
 
 # views
-def home(request):
+def index(request):
+    location= Location.objects.all()
     images= Image.objects.all()
-    return render(request, 'my_gram/home.html', {'images':images})
+    title='Picsabay'
+    return render(request, 'my_gram/index.html',{'title':title,'images':images, 'location':location}})
+
+
+def location_filter(request, image_location):
+    locs = Location.get_location_id(image_location)
+    location = Location.objects.all()
+    images = Image.filter_by_location(image_location)
+    title = f'Picsabay {location}'
+    return render(request, 'my_gram/location.html', {'title':title, 'images':images, 'location':location, 'locs':locs})
+
+def single(request,category_name,image_id):
+    # images
+    title='Picsabay'
+    location=Location.objects.all()
+    # categories
+    category = Image.objects.filter(category__name = category_name)
+    try:
+        image = Image.objects.get(id=image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"my_gram/single.html",{'title':title,"image":image, "location":location, "category":category})
+
+def search_image(request):
+    title = 'Search Picsabay'
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get('category')
+        found_results = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        return render(request, 'my_gram/search.html',{'title':title,'images': found_results, 'message': message, 'categories': categories, "locations":locations})
+    else:
+        message = "Didn't find anything to search"
+        return render(request, 'my_gram/search.html',{"message": message})
