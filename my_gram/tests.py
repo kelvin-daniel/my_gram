7c8a995e15c3377c9544baf7c44c09a93e1c3408
kelvin-daniel <kaymutor@gmail.com>
2020-10-10 14:04:10 +0300

    run more tests on instantiation

--- my_gram/tests.py
+++ my_gram/tests.py
@@ -4,7 +4,26 @@ from .models import Image,Category,Location
 # tests
 class TestImage(TestCase):
     def setUp(self):
+        #location test
         self.location=Location(name='Mali')
         self.location.save_location()
-        sel
+        #category test
+        self.category= Category(category='art')
+        self.category.save_category()
+        #image test
         self.image = Image(name='creekidy', description='creekidy rock on creek road', category=self.category, location=self.location)
+        self.image.save_image()
+
+    def test_save_image(self):
+        self.image.save()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
+    
+    def test_delete_image(self):
+        self.image.delete()
+        images = Image.objects.all()
+        self.assertTrue(len(images)==0)
+    def test_update_image(self):
+        pass
+    def test_get_image_by_id(self)
+    
