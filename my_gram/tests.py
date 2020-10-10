d38ccee773955c51f6a0c9c3dc67102287576fad
kelvin-daniel <kaymutor@gmail.com>
2020-10-10 15:34:09 +0300

    test locations

--- my_gram/tests.py
+++ my_gram/tests.py
@@ -27,3 +27,52 @@ class TestImage(TestCase):
         pass
     def test_get_image_by_id(self)
     
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.categry = Category(name="art")
+        self.categry.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.categry, Category))
+
+    def test_save_method(self):
+        self.categry.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+    def test_delete_method(self):
+        self.categry.save_category()
+        self.categry.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)
+
+    def test_update(self):
+        category = Category.get_category_id(self.categry.id)
+        category.update_category('paint')
+        category = Category.get_category_id(self.categry.id)
+        self.assertTrue(category.name == 'Paint')
+
+class LocationTestCLass(TestCase):
+    def setUp(self):
+        self.locashon = Location(name="bahamas")
+        self.locashon.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.locashon,Location))
+
+    def test_save_method(self):
+        self.locashon.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_delete_method(self):
+        self.locashon.save_location()
+        self.locashon.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
+
+    def test_update(self):
+        location = Location.get_location_id(self.locashon.id)
+        location.update_location('panama')
+        location = Location.get_location_id(self.locashon.id)
+        self.assertTrue(location.name == 'panama')
