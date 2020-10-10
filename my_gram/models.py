c5d88895903f6fd6c10e29293000355954a17b6b
kelvin-daniel <kaymutor@gmail.com>
2020-10-10 13:38:54 +0300

    create image,location,category models

--- my_gram/models.py
+++ my_gram/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+# app models
+class Image(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    category = models.ForeignKey('category', on_delete=models.CASCADE,)
+    location = models.ForeignKey('location', on_delete=models.CASCADE,)
+    image = CloudinaryField('image')
+    def save_image(self):
+        self.save()
+    def update_image(self):
+        self.update()
+    def delete_image(self):
+        self.delete()
+
+    class Meta:
+        verbose_name='Image'
+        verbose_name_plural='Images'
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    category= models.CharField(max_length=20)
+    def save_category(self):
+        self.save()
+    def update_cateory(self):
+        self.update()
+    def delete_category(self):
+        self.delete()
+        pass
+
+class Location(models.Model):
+    name= models.CharField(max_length=20)
+    def save_location(self):
+        self.save()
+    def update_location(self):
+        self.update()
+    def delete_location(self):
+        self.delete()
+        pass
