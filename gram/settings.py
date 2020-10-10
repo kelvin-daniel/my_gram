bce2e4880ae88c5b3d313d4c51faa6fb985af303
kelvin-daniel <kaymutor@gmail.com>
2020-10-10 12:55:50 +0300

    move secret key to environment

--- gram/settings.py
+++ gram/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o93$&+p!byv&jxavaq_dn*34_9a4h6ojv7=pt5%t@%#mgr%hi='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
